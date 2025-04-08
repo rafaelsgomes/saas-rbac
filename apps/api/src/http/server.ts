179d0a4dc4bef6ea19b3980823de8ccd802bcef0
Rafael Gomes <rafaelsgomes.dev@gmail.com>
2025-04-08 10:51:12 -0300

    chore: setup 0.0.0.0 as default api host

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -114,6 +114,6 @@ app.register(GetPendingInvites)
 
 app.register(GetOrganizationBilling)
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
 })
