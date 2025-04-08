7a73454058110cba6009141a1668dea7b173ca0e
Rafael Gomes <rafaelsgomes.dev@gmail.com>
2025-04-08 13:31:40 -0300

    fix: useParams import

--- apps/web/src/app/invite/[id]/page.tsx
+++ apps/web/src/app/invite/[id]/page.tsx
@@ -1,9 +1,8 @@
-'use client'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import { CheckCircle, LayoutDashboard, LogIn, LogOut } from 'lucide-react'
 import Link from 'next/link'
-import { redirect, useParams } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 import { auth, isAuthenticated } from '@/auth/auth'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
@@ -14,8 +13,14 @@ import { getInvite } from '@/http/get-invite'
 
 dayjs.extend(relativeTime)
 
+const getInviteId = async () => {
+  'use client'
+  const useParams = (await import('next/navigation')).useParams
+  return useParams<{ id: string }>()
+}
+
 export default async function InvitePage() {
-  const { id: inviteId } = useParams<{ id: string }>()
+  const { id: inviteId } = await getInviteId()
   const { invite } = await getInvite(inviteId)
   const isUserAuthenticated = await isAuthenticated()
   const { user } = await auth()
